9bb01306896e46b4141f651f0c3ffb19fefaa856
PodMiu <voann@192.168.1.13.non-exists.ptr.local>
2024-12-07 20:26:14 +0700

    triangle

--- triangle.py
+++ triangle.py
@@ -1,7 +1,7 @@
 canh = int(input("Nhap chieu danh canh"))
 k = 0
 for i in range(canh):
-    for w in range((canh-1-i)*2):
+    for w in range((canh-i-1)*2):
         print(" ",end="")
     for a in range(i+1):
         print(a+1,"",end="")
